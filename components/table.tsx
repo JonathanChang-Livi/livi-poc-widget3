08bdfe854da48521f1527749f88a9c40095f2715
jonathanchang <cpk826@gmail.com>
2023-01-13 16:18:10 +0800

    Update table.tsx

--- components/table.tsx
+++ components/table.tsx
@@ -13,12 +13,12 @@ export interface TableHead {
 }
 
 const TableHeader = ({ header, className, override, style }: TableHeaderProps & ComponentProps) => {
-    const headClass = 'justify-around w-full h-6 mb-5 grid grid-cols-12'
+    const headClass = 'w-full h-6 mb-5 grid grid-cols-12'
     return (
         <Group className={`${override && override ? '' : headClass} ${className || ''}`} style={style}>
             {header.map((head, i) => {
                 return (
-                    <div className={`col-span-${head.width} flex p-2`}>
+                    <div className={`col-span-${head.width} p-2`}>
                         <p className={`w-full text-base font-semibold text-slate-500 text-${head.position || 'left'} `}>{head.value}</p>
                     </div>
                 )
@@ -33,13 +33,13 @@ export interface TableRowProps {
 }
 
 const TableRow = ({ field, header, className, override, style }: TableRowProps & ComponentProps) => {
-    const headClass = 'justify-around w-full grid grid-cols-12'
+    const headClass = 'w-full grid grid-cols-12'
     return (
         <Group className={`${override && override ? '' : headClass} ${className || ''}`} style={style}>
             {<></>}
             {field.map((f, i) => {
                 return (
-                    <div className={`flex p-2 col-span-${header[i].width}`}>
+                    <div className={`p-2 col-span-${header[i].width}`}>
                         {f}
                     </div>
                 )
